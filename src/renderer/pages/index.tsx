c3968ced6e74694ed925d9e9cc534c02f0a1f44f
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2021-02-16 14:08:02 -0800

    feat: fetch questions and display

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -1,7 +1,27 @@
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+const IndexPage = () => {
+  const [data, setData] = useState<Array<string>>([]);
 
-const IndexPage = () => (
-  <Layout title="Questions and Answer app">[todo: list of questions]</Layout>
-);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('http://localhost:8000/questions');
+        const data = await response.json();
+        setData(data as Array<string>);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    return () => {};
+  }, []);
+  return (
+    <Layout title="Questions and Answer app">
+      {data.map((question: string) => (
+        <button key={question}>{question}</button>
+      ))}
+    </Layout>
+  );
+};
 
 export default IndexPage;
