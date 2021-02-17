2af6212e76c575ffe0a6288be595dc426b049fbb
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2021-02-16 16:02:40 -0800

    refactor: remove compile js from being tracked

--- dist/renderer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const { ipcRenderer } = require('electron');
-ipcRenderer.on('asynchronous-reply', (event, arg) => {
-    console.log(arg);
-});
-ipcRenderer.send('open-window', 'ping');
-//# sourceMappingURL=renderer.js.map
